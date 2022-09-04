1380dae1b4788369cfef64e6ecb67aae17165d98
Rustam Bikmukhametov <rbikmukhametov@diasoft.ru>
2022-09-04 23:07:49 +0300

    changed to GTest

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,17 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include "googletest/googlemock/include/gmock/gmock.h"
+#include "googletest/googletest/include/gtest/gtest.h"
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(TestGroupName, Subtest_1) {
+  ASSERT_TRUE(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+
+int main(int argc, char **argv)
+{
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
